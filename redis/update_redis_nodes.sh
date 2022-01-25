e0844857d03a101738b0a3a8f9f3873c66ce6a42
IT.SOS <peng.yu@qjfu.cn>
2022-01-25 10:05:38 +0800

    [更新] 恢复redis连接脚本 [新增] redis新增对redisjson、redisearch的支持

--- redis/update_redis_nodes.sh
+++ redis/update_redis_nodes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true;do
-    isOk=$(kubectl -n redis-cluster get statefulset.apps/redis-app|grep -v 'NAME'|awk '{print $2}'|awk -F / '{if ("$2"-"$1"==0) {print "true"}}')
+    isOk=$(kubectl -n redis-cluster3 get statefulset.apps/redis-app|grep -v 'NAME'|awk '{print $2}'|awk -F / '{if ("$2"-"$1"==0) {print "true"}}')
     if [ "$isOk" = "true" ];then
         break
     fi
@@ -10,8 +10,8 @@ while true;do
 done
 
 j=""
-for i in 0 1 2 3 4 5 6 7 8; do
-    j=$(kubectl exec -it redis-app-$i -n redis-cluster -- redis-cli cluster nodes|grep myself|awk '{print $1" "$2}')"|"$j
+for i in 0 1 2 3 4 5; do
+    j=$(kubectl exec -it redis-app-$i -n redis-cluster3 -- redis-cli cluster nodes|grep myself|awk '{print $1" "$2}')"|"$j
 done
 
 oldIFS=$IFS
@@ -19,11 +19,13 @@ IFS="|"
 s=""
 for n in $j; do
     id=$(echo "$n"|cut -d " " -f 1)
-    s="sed -i -e 's/${id} .*:6379@16379/$n/' /var/lib/redis/nodes.conf;$s"
+    if [ ! -z "$id" ]; then
+        s="sed -i -e 's/${id} .*:6379@16379/$n/' /var/lib/redis/nodes.conf;$s"
+    fi
 done
 IFS=$oldIFS
 
-kubectl exec -it redis-app-0 -n redis-cluster -- sh -c "$s"
+kubectl exec -it redis-app-0 -n redis-cluster3 -- sh -c "$s"
 if [ "$?" -eq "0" ]; then
-    kubectl -n redis-cluster delete pod redis-app-0
+    kubectl -n redis-cluster3 delete pod redis-app-0
 fi
