a2329535c9d750705f8b4e089c96bc246f8690d0
IT.SOS <peng.yu@qjfu.cn>
2022-04-01 10:38:59 +0800

    update.

--- redis/update_redis_nodes.sh
+++ redis/update_redis_nodes.sh
@@ -1,31 +1,48 @@
 #!/bin/sh
 
+export KUBECONFIG=/etc/kubernetes/admin.conf
+logfile=/tmp/update_redis_nodes.log
+
+echo $(date)': reconnect redis cluster.'|tee $logfile
+
 while true;do
-    isOk=$(kubectl -n redis-cluster3 get statefulset.apps/redis-app|grep -v 'NAME'|awk '{print $2}'|awk -F / '{if ("$2"-"$1"==0) {print "true"}}')
-    if [ "$isOk" = "true" ];then
-        break
-    fi
-    sleep 10
-    continue
-done
+  while true;do
+      isOk=$(kubectl -n redis-cluster get statefulset.apps/redis-app|grep -v 'NAME'|awk '{print $2}'|awk -F / '{if ("$2"-"$1"==0) {print "true"}}')
+      if [ "$isOk" = "true" ];then
+          break
+      fi
+      sleep 60
+      continue
+  done
+  
+  j=""
+  for i in 0 1 2 3 4 5; do
+      j=$(kubectl exec -i redis-app-$i -n redis-cluster -- redis-cli cluster nodes|grep myself|awk '{print $1" "$2}')"|"$j
+  done
+  
+  oldIFS=$IFS
+  IFS="|"
+  s=""
+  for n in $j; do
+      id=$(echo "$n"|cut -d " " -f 1)
+      if [ ! -z "$id" ]; then
+          s="sed -i -e 's/${id} .*:6379@16379/$n/' /var/lib/redis/nodes.conf;$s"
+      fi
+  done
+  IFS=$oldIFS
+  pod=$(kubectl -n redis-cluster get po|grep 1/1|head -n 1|awk '{print $1}')
+  kubectl exec -i $pod -n redis-cluster -- sh -c "$s"
+  if [ "$?" -eq "0" ]; then
+      kubectl -n redis-cluster delete pod $pod
+  fi
 
-j=""
-for i in 0 1 2 3 4 5; do
-    j=$(kubectl exec -it redis-app-$i -n redis-cluster3 -- redis-cli cluster nodes|grep myself|awk '{print $1" "$2}')"|"$j
-done
+  sleep 30
 
-oldIFS=$IFS
-IFS="|"
-s=""
-for n in $j; do
-    id=$(echo "$n"|cut -d " " -f 1)
-    if [ ! -z "$id" ]; then
-        s="sed -i -e 's/${id} .*:6379@16379/$n/' /var/lib/redis/nodes.conf;$s"
-    fi
+  ok=$(kubectl exec -i redis-app-0 -n redis-cluster -- redis-cli cluster info|head -n 1|awk '{print $1}'|sed -e 's/[ \t\n\r]*//g')
+  if [ "$ok"x = "cluster_state:ok"x ]; then
+      echo $(date)": check is ok." |tee -a $logfile
+      break
+  else
+      echo $(date)": check is fail. loop." |tee -a $logfile
+  fi
 done
-IFS=$oldIFS
-
-kubectl exec -it redis-app-0 -n redis-cluster3 -- sh -c "$s"
-if [ "$?" -eq "0" ]; then
-    kubectl -n redis-cluster3 delete pod redis-app-0
-fi
